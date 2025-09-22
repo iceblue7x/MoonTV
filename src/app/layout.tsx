10b31f212fdea255f24f3a38a11ec2dff76adc25
Sam <iceblue7x@gmail.com>
2025-09-22 23:42:20 +0800

    update

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -108,7 +108,7 @@ export default async function RootLayout({
       <body
         className={`${inter.className} min-h-screen bg-white text-gray-900 dark:bg-black dark:text-gray-200`}
       >
-        {/* Google Cast Sender SDK */}
+        {/* Add Google Cast Sender SDK */}
         <Script
           src="https://www.gstatic.com/cv/js/sender/v1/cast_sender.js?loadCastFramework=1"
           strategy="beforeInteractive"
